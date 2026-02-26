f3713e72668308b05febbf860e79d41c8a596726
14088169 <JimingDong2007@outlook.com>
2026-02-27 01:17:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from product import Product
+from inventory import Inventory
+from user import User
 
+if __name__ == "__main__":
+    apple = Product("abc", "苹果", 5.99)
+    stock = Inventory()
+    admin = User("U001", "管理员", "admin")
+
+    print(apple.get_basic_info())
