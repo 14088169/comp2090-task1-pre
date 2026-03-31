309eb36f1e48732f16622625d58bb6bdad5ce955
14088169 <JimingDong2007@outlook.com>
2026-04-01 02:12:19 +0800

    Update matrix.py

--- task2/matrix.py
+++ task2/matrix.py
@@ -1,19 +1,39 @@
 class Matrix:
-    def __init__(self, rows, cols, value=0):
-        self.rows = rows
-        self.cols = cols
-        self.data = [[value for _ in range(cols)] for _ in range(rows)]
+    def __init__(self, data):
+        if not all(isinstance(row, list) for row in data):
+            raise ValueError("矩阵必须由列表组成")
+        row_length = len(data[0])
+        if not all(len(row) == row_length for row in data):
+            raise ValueError("矩阵每一行的元素个数必须相同")
+        
+        self.data = data
+        self.rows = len(data)
+        self.cols = row_length
 
-    def get(self, i, j):
-        return self.data[i][j]
+    def __str__(self):
+        return '\n'.join(['\t'.join(map(str, row)) for row in self.data])
 
-    def set(self, i, j, value):
-        self.data[i][j] = value
+    def __add__(self, other):
+        if self.rows != other.rows or self.cols != other.cols:
+            raise ValueError("两个矩阵的行列数必须相同才能相加")
+        
+        result = []
+        for i in range(self.rows):
+            new_row = [self.data[i][j] + other.data[i][j] for j in range(self.cols)]
+            result.append(new_row)
+        return Matrix(result)
 
-    def display(self):
-        for row in self.data:
-            print(row)
+    def __mul__(self, other):
+        if self.cols != other.rows:
+            raise ValueError("第一个矩阵的列数必须等于第二个矩阵的行数")
+        
+        result = [[0 for _ in range(other.cols)] for _ in range(self.rows)]
+        for i in range(self.rows):
+            for j in range(other.cols):
+                for k in range(self.cols):
+                    result[i][j] += self.data[i][k] * other.data[k][j]
+        return Matrix(result)
 
-
-m = Matrix(4, 3)
-m.display()
+    def transpose(self):
+        result = [[self.data[j][i] for j in range(self.rows)] for i in range(self.cols)]
+        return Matrix(result)
